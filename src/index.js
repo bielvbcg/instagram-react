abca827babf4e90df2b364e7b4fed33b9f7223a2
bielvbcg <gabrielvbcallegario@gmail.com>
2021-12-04 00:18:04 -0300

    Componentizaçao inicial feita

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert("teste")
+import  ReactDOM  from "react-dom";
+import FundoMobile from "./jsx/FundoMobile";
+import Corpo from "./jsx/Corpo";
+import Navbar from "./jsx/Navbar";
+
+function App(){
+  return (
+    <div>
+      <Navbar />
+      <Corpo />
+      <FundoMobile />
+    </div>
+  );
+}
+
+ReactDOM.render(<App /> , document.querySelector(".root"));
