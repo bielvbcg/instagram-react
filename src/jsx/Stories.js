155fb529252903ae211eeab71294407e9e90dcca
bielvbcg <gabrielvbcallegario@gmail.com>
2021-12-04 02:00:40 -0300

    stories totalmente componentizados

--- src/jsx/Stories.js
+++ src/jsx/Stories.js
@@ -1,80 +1,63 @@
 export default function Stories(){
+  const infoStories = [
+    {
+      imagem: "assets/img/9gag.svg",
+      usuario: "9gag",
+    },
+    {
+      imagem: "assets/img/meowed.svg",
+      usuario: "meowed",
+    },
+    {
+      imagem: "assets/img/barked.svg",
+      usuario: "barked",
+    },
+    {
+      imagem: "assets/img/nathanwpylestrangeplanet.svg",
+      usuario: "nathanwpylestrangeplanet",
+    },
+    {
+      imagem: "assets/img/wawawicomics.svg",
+      usuario: "wawawicomics",
+    },
+    {
+      imagem: "assets/img/respondeai.svg",
+      usuario: "respondeai",
+    },
+    {
+      imagem: "assets/img/filomoderna.svg",
+      usuario: "filomoderna",
+    },
+    {
+      imagem: "assets/img/memeriagourmet.svg",
+      usuario: "memeriagourmet",
+    },
+  ]
+
   return (
     <div class="stories">
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/9gag.svg" />
-        </div>
-        <div class="usuario">
-          9gag
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/meowed.svg" />
-        </div>
-        <div class="usuario">
-          meowed
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/barked.svg" />
-        </div>
-        <div class="usuario">
-          barked
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/nathanwpylestrangeplanet.svg" />
-        </div>
-        <div class="usuario">
-          nathanwpylestrangeplanet
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/wawawicomics.svg" />
-        </div>
-        <div class="usuario">
-          wawawicomics
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/respondeai.svg" />
-        </div>
-        <div class="usuario">
-          respondeai
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/filomoderna.svg" />
-        </div>
-        <div class="usuario">
-          filomoderna
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/memeriagourmet.svg" />
-        </div>
-        <div class="usuario">
-          memeriagourmet
-        </div>
-      </div>
+      {infoStories.map( info => {
+        <Story 
+          imagem={info.imagem}
+          usuario={info.usuario}
+        />
+      })}
 
       <div class="setinha">
         <ion-icon name="chevron-forward-circle"></ion-icon>
+      </div>
+    </div>
+  )
+}
+
+function Story(){
+  return (
+    <div class="story">
+      <div class="imagem">
+        <img src= {imagem}/>
+      </div>
+      <div class="usuario">
+        {usuario}
       </div>
     </div>
   )
